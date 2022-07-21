72ec9a988002ecb468c9f9ef18cbab188f923a89
bhargav27b <bhargav@sketchbrahma.com>
2022-07-21 16:59:37 +0530

    using props to pass the data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from "react";
 import Header from "./components/Header"
 
-class App extends React.Component {
-  render() {
-    return(
-      <Header />
-    )
-  }
+function App() {
+  return (
+    <>
+      <Header title="task tracker" />
+    </>
+  );
 }
 
-export default App
+export default App;
